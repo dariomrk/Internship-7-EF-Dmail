37d5af8a7efedeee3709d7d1a8920b2d7bd50b87
Dario <dariomrkonjic1@gmail.com>
2022-12-22 14:02:39 +0100

    Add project files.

---
+++ Internship-7-EF-Dmail.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Internship_7_EF_Dmail.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
