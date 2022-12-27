952bd06470e3643284e4939d86ff6ec7caaf7e91
Dario <dariomrkonjic1@gmail.com>
2022-12-27 13:55:24 +0100

    Started tinkering with the DBContext. First successful migration.

--- Internship-7-EF-Dmail.Data/Context/DmailDBContext.cs
+++ Internship-7-EF-Dmail.Data/Context/DmailDBContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Internship_7_EF_Dmail.Data.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Internship_7_EF_Dmail.Data.Context
 {
@@ -6,6 +9,47 @@ namespace Internship_7_EF_Dmail.Data.Context
     {
         public DmailDBContext(DbContextOptions options) : base(options)
         {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Mail> Mails => Set<Mail>();
+        public DbSet<Recipient> Recipients => Set<Recipient>();
+        public DbSet<SpamFlag> SpamFlags => Set<SpamFlag>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SpamFlag>()
+                .HasKey(sf => new { sf.UserId, sf.FlaggedUserId });
+
+            modelBuilder.Entity<SpamFlag>()
+                .HasOne(sf => sf.User)
+                .WithMany(u => u.Flagged)
+                .HasForeignKey(sf => sf.UserId);
+
+            modelBuilder.Entity<Recipient>()
+                .HasKey(r => new { r.MailId, r.UserId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    public class DmailDBContextFactory : IDesignTimeDbContextFactory<DmailDBContext>
+    {
+        public DmailDBContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("App.config")
+                .Build();
+
+            config.Providers
+                .First()
+                .TryGet("connectionStrings:add:DmailApp:connectionString", out var connectionString);
+
+            var options = new DbContextOptionsBuilder<DmailDBContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+            
+            return new DmailDBContext(options);
         }
     }
 }
---
+++ Internship-7-EF-Dmail.Data/Migrations/20221227125346_InitMigration.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Internship_7_EF_Dmail.Data.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Rights = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Format = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    EventStartAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EventDuration = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    SenderId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mails_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpamFlags",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    FlaggedUserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpamFlags", x => new { x.UserId, x.FlaggedUserId });
+                    table.ForeignKey(
+                        name: "FK_SpamFlags_Users_FlaggedUserId",
+                        column: x => x.FlaggedUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SpamFlags_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipients",
+                columns: table => new
+                {
+                    MailId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    EventStatus = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipients", x => new { x.MailId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Recipients_Mails_MailId",
+                        column: x => x.MailId,
+                        principalTable: "Mails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Recipients_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mails_SenderId",
+                table: "Mails",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipients_UserId",
+                table: "Recipients",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpamFlags_FlaggedUserId",
+                table: "SpamFlags",
+                column: "FlaggedUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipients");
+
+            migrationBuilder.DropTable(
+                name: "SpamFlags");
+
+            migrationBuilder.DropTable(
+                name: "Mails");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
