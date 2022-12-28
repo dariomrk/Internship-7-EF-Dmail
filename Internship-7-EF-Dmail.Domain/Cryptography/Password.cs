e0731b648a4e85ef32434c01feebe28083a0c808
Dario <dariomrkonjic1@gmail.com>
2022-12-28 18:19:51 +0100

    Started work on the Domain layer.

---
+++ Internship-7-EF-Dmail.Domain/Cryptography/Password.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+
+namespace Internship_7_EF_Dmail.Domain.Cryptography
+{
+    static public class Password
+    {
+        private const byte _saltSize = 16;
+        private const byte _keySize = 32;
+        private const ushort _iterations = 10000;
+        private static readonly HashAlgorithmName _algorithm = HashAlgorithmName.SHA256;
+
+        public static string Hash(string input)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(_saltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(
+                input,
+                salt,
+                _iterations,
+                _algorithm,
+                _keySize
+            );
+
+            return string.Join(
+                "_",
+                Convert.ToHexString(hash),
+                Convert.ToHexString(salt),
+                _iterations,
+                _algorithm
+            );
+        }
+        public static bool Verify(string input, string hashString)
+        {
+            string[] segments = hashString.Split("_");
+
+            byte[] hash = Convert.FromHexString(segments[0]);
+            byte[] salt = Convert.FromHexString(segments[1]);
+            int iterations = int.Parse(segments[2]);
+
+            HashAlgorithmName algorithm = new HashAlgorithmName(segments[3]);
+
+            byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(
+                input,
+                salt,
+                iterations,
+                algorithm,
+                hash.Length
+            );
+
+            return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+        }
+    }
+}
---
+++ Internship-7-EF-Dmail.Domain/Factories/DBContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Internship_7_EF_Dmail.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace Internship_7_EF_Dmail.Domain.Factories
+{
+    public static class DBContextFactory
+    {
+        public static DmailDBContext GetDmailDBContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql(ConfigurationManager.ConnectionStrings["Dmail"].ConnectionString)
+                .Options;
+
+            return new DmailDBContext(options);
+        }
+    }
+}
