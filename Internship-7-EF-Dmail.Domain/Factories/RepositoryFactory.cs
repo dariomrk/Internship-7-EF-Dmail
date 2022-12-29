7100b24cbe751992849b99767d9e3694b16830d2
Dario <dariomrkonjic1@gmail.com>
2022-12-29 18:05:34 +0100

    Written RepositoryFactory.

---
+++ Internship-7-EF-Dmail.Domain/Factories/RepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using Internship_7_EF_Dmail.Data.Context;
+using Internship_7_EF_Dmail.Domain.Repos;
+
+namespace Internship_7_EF_Dmail.Domain.Factories
+{
+    public static class RepositoryFactory
+    {
+        public static TRepo GetRepository<TRepo>() where TRepo : BaseRepo
+        {
+            DmailDBContext context = DBContextFactory.GetDmailDBContext();
+            var repository = Activator.CreateInstance(typeof(TRepo),context) as TRepo;
+
+            return repository!;
+        }
+    }
+}
