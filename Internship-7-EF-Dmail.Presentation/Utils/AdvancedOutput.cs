5939993d3eb28409a8c68295d232d9ab9cb1125f
Dario <dariomrkonjic1@gmail.com>
2023-01-03 13:03:44 +0100

    Written AdvancedOutput util.

---
+++ Internship-7-EF-Dmail.Presentation/Utils/AdvancedOutput.cs
@@ -0,0 +1,72 @@
+﻿namespace Internship_7_EF_Dmail.Presentation.Utils
+{
+    public static class AdvancedOutput
+    {
+        public enum Style
+        {
+            Standard,
+            Success,
+            Warning,
+            Error,
+        }
+
+        public struct StyleSettings
+        {
+            public ConsoleColor Foreground { get; init; }
+            public ConsoleColor Background { get; init; }
+            public string PrependMessage { get; init; }
+        }
+
+        private static StyleSettings GetStyleSettings(Style style)
+        {
+            return style switch
+            {
+                Style.Standard => new StyleSettings
+                {
+                    Foreground = ConsoleColor.White,
+                    Background = ConsoleColor.Black,
+                    PrependMessage = "",
+                },
+                Style.Success => new StyleSettings
+                {
+                    Foreground = ConsoleColor.White,
+                    Background = ConsoleColor.DarkGreen,
+                    PrependMessage = "Success: ",
+                },
+                Style.Warning => new StyleSettings
+                {
+                    Foreground = ConsoleColor.Black,
+                    Background = ConsoleColor.DarkYellow,
+                    PrependMessage = "Warning: ",
+                },
+                Style.Error => new StyleSettings
+                {
+                    Foreground = ConsoleColor.White,
+                    Background = ConsoleColor.DarkRed,
+                    PrependMessage = "Error! ",
+                },
+                _ => throw new ArgumentOutOfRangeException(),
+            };
+        }
+
+        public static void Write(string message, Style writeStyle = Style.Standard)
+        {
+            var style = GetStyleSettings(writeStyle);
+
+            Console.ForegroundColor = style.Foreground;
+            Console.BackgroundColor = style.Background;
+            Console.Write(style.PrependMessage + message);
+            Console.ResetColor();
+        }
+
+        public static void WriteLine(string message, Style writeStyle = Style.Standard)
+        {
+            var style = GetStyleSettings(writeStyle);
+
+            Console.ForegroundColor = style.Foreground;
+            Console.BackgroundColor = style.Background;
+            Console.WriteLine(style.PrependMessage + message);
+            Console.ResetColor();
+        }
+    }
+}
