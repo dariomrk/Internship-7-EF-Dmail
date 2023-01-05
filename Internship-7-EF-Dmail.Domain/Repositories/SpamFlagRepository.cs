bae5940a6a792983161d6851404a7010c24e3694
Dario <dariomrkonjic1@gmail.com>
2023-01-05 18:13:55 +0100

    Added SpamFlagRepository.

---
+++ Internship-7-EF-Dmail.Domain/Repositories/SpamFlagRepository.cs
@@ -0,0 +1,58 @@
+﻿using Internship_7_EF_Dmail.Data.Context;
+using Internship_7_EF_Dmail.Data.Models;
+using Internship_7_EF_Dmail.Domain.Enums;
+using Internship_7_EF_Dmail.Domain.Repositories.Interfaces;
+
+namespace Internship_7_EF_Dmail.Domain.Repositories
+{
+    public class SpamFlagRepository : BaseRepository, IRepository<SpamFlag>
+    {
+        public SpamFlagRepository(DmailDBContext context) : base(context)
+        {
+        }
+        public ICollection<SpamFlag> GetAll() => context.SpamFlags.ToList();
+
+        public SpamFlag? GetById(int id)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Response Add(SpamFlag entity)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Response Delete(int id)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Response Update(SpamFlag entity)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Response MarkAsSpam(int userId, int userToFlag)
+        {
+            context.SpamFlags.Add(new SpamFlag()
+            {
+                UserId = userId,
+                FlaggedUserId = userToFlag,
+            });
+
+            return base.SaveChanges();
+        }
+
+        public Response RemoveAsSpam(int userId, int userToFlag)
+        {
+            context.SpamFlags.Remove(new SpamFlag()
+            {
+                UserId = userId,
+                FlaggedUserId = userToFlag,
+            });
+
+            return base.SaveChanges();
+        }
+
+    }
+}
